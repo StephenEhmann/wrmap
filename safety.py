c2182f289dd3ef656bf4e456332f148ace3cfe8e
StephenEhmann <sehmann@gmail.com>
2018-01-26 13:29:03 -0800

    features: main wrmap.py(geocode, eval), location.yml, skeletons for more modulesl

---
+++ safety.py
@@ -0,0 +1,136 @@
+#!/home/utils/Python-3.6.1/bin/python3
+
+import os
+import sys
+import subprocess
+import tempfile
+import time
+import traceback
+import argparse
+import glob
+import re
+import time
+from datetime import datetime
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+import googlemaps
+gmaps = googlemaps.Client(key='AIzaSyDNyA5ZDP1JClw9sTnVXuFJP_1FvZk30zU')
+
+import utils
+
+criterionName = 'safety'
+
+def find(location, bounds, data, name=None):
+    pass
+
+def init():
+    with open(criterionName + '.yml', 'r') as in_file:
+        data = load(in_file, Loader=Loader)
+    return data
+
+def evaluate(loc, data, threshold):
+    metric = threshold['metric']
+    better = threshold['better']
+    mn = threshold['min']
+    mx = threshold['max']
+    if (metric != 'distance_to_nearest'):
+        raise Exception('ERROR: can only evaluate for metric distance_to_nearest')
+
+    minDist = 100
+    minK = ''
+    for k, v in iter(data.items()):
+        dist = utils.distance(loc, (v['location']['lat'], v['location']['lng']))
+        if (dist < minDist):
+            minDist = dist
+            minK = k
+    return utils.ramp(minDist, mn, mx, (better != 'lower'))
+
+
+if (__name__ == "__main__"):
+    # main parser
+    class MainParser(argparse.ArgumentParser):
+        def error(self, message):
+            self.print_help()
+            sys.stderr.write('error: %s\n' % message)
+            sys.exit(2)
+    parser = MainParser(add_help=False, allow_abbrev=False)
+    parser.add_argument('-h', action='store_true', help=argparse.SUPPRESS)
+    parser.add_argument('--h', action='store_true', help=argparse.SUPPRESS)
+    parser.add_argument('-help', action='store_true', help=argparse.SUPPRESS)
+    parser.add_argument('--help', action='store_true', help=argparse.SUPPRESS)
+    parser.add_argument('-debug', action='store_true', help='print extra info')
+    parser.add_argument('-find', action='store_true', help='find all DB items and write them to ' + criterionName + '.yml')
+    parser.add_argument('-name', type=str, help='find only for this name')
+    parser.add_argument('-evaluate', type=str, help='evaluate the ' + criterionName + ' score for a given coordinate pair (eg. -evaluate 35.936164,-79.040997)')
+    args = parser.parse_args()
+
+    if (args.h or args.help):
+        parser.print_help()
+        sys.exit(0)
+
+    if (args.find and args.evaluate or not args.find and not args.evaluate):
+        print('ERROR: exactly one action must be given, choose exactly one of -find or -evaluate')
+        sys.exit(1)
+
+    if (not args.find and args.name):
+        print('ERROR: -name can only be used with -find')
+        sys.exit(1)
+
+    # read locations
+    try:
+        stream = open('location.yml', 'r')
+        locations = load(stream, Loader=Loader)
+        stream.close()
+    except Exception as e:
+        print(str(e))
+        raise Exception('ERROR: Failed to load yaml file ' + 'config.yml')
+
+    # read config
+    try:
+        stream = open('config.yml', 'r')
+        config = load(stream, Loader=Loader)
+        stream.close()
+    except Exception as e:
+        print(str(e))
+        raise Exception('ERROR: Failed to load yaml file ' + 'config.yml')
+
+    location = locations[config['location']] ['location']
+    bounds = locations[config['location']] ['bounds']
+
+    if (args.find):
+        data = {}
+        find(location, bounds, data, args.name)
+        if (args.name == None):
+            for i in config[criterionName]['include'].keys():
+                find(location, bounds, data, i)
+
+        deletions = []
+        for k, v in iter(data.items()):
+            if (config[criterionName]['exclude'].get(v['name'])):
+                print('deleting ' + k)
+                deletions.append(k)
+
+        for d in deletions:
+            del data[d]
+
+        if (args.name == None):
+            modName = ''
+        else:
+            modName = re.sub(r'\s', r'_', args.name) + '.'
+
+        with open(criterionName + '.' + modName + 'yml', 'w') as yaml_file:
+            dump(data, yaml_file, default_flow_style=False, Dumper=Dumper)
+
+    elif (args.evaluate):
+        data = init()
+        latlong = args.evaluate.split(',')
+        e = evaluate((latlong[0], latlong[1]), data, config['evaluation']['threshold'][criterionName])
+        print(str(e))
+
+    sys.exit(0)
+
+# test:
