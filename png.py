69823860ad427f886b736cc247ba7d3eefe6b2eb
StephenEhmann <sehmann@gmail.com>
2018-03-14 06:38:19 -0700

    hsv color gradients

--- png.py
+++ png.py
@@ -10,7 +10,7 @@ import argparse
 import glob
 import re
 import time
-import importlib
+import colorsys
 from datetime import datetime
 from yaml import load, dump
 try:
@@ -21,6 +21,16 @@ except ImportError:
 from PIL import Image
 
 import utils
+
+# https://en.wikipedia.org/wiki/Hue
+# TODO: feature to add: variable range lengths like this:
+#    'red_green': { 'ranges': 10, 'ranges': [(0.0, 255,0,0), (0.3, 255,255,0), (1.0, 0,255,0)] },
+quant = {
+    #RETIRED because red_green_hue does the same thing... 'red_yellow_green': { 'mode': 'rgb', 'numValues': 10, 'ranges': [ [(255,0,0), (255,255,0)], [(255,255,0), (0,255,0)] ] },
+    'red_white_green': { 'mode': 'hsv', 'blend': 's', 'numValues': 10, 'ranges': [ [(0.0,1.0,1.0), (0.0,0.0,1.0)], [(120.0/360.0,0.0,1.0), (120.0/360.0,1.0,1.0)] ] },
+    'red_white_blue': { 'mode': 'hsv', 'blend': 's', 'numValues': 10, 'ranges': [ [(0.0,1.0,1.0), (0.0,0.0,1.0)], [(240.0/360.0,0.0,1.0), (240.0/360.0,1.0,1.0)] ] },
+    'red_green_hue': { 'mode': 'hsv', 'blend': 'h', 'numValues': 10, 'ranges': [ [(0.0,1.0,1.0), (120.0/360.0,1.0,1.0)] ] },
+}
 
 def write(filename, config, results, width, height):
     im = Image.new("RGB", (width, height))
@@ -37,62 +47,8 @@ def write(filename, config, results, width, height):
             green = 255
 
         val = (red, green, blue)
-        #        int(255 * (1.0 - v)),
-        #        int(255 * v),
-        #        0
-        #      )
         data[x,height-y-1] = val
     im.save(filename, "PNG")
-
-    if (0):
-        im = Image.new("RGB", (2, 2))
-        pix = im.load()
-        for x in range(2):
-            for y in range(2):
-                pix[x,y] = (255,0,0)
-        im.save(filename, "PNG")
-
-    if (0):
-        data = bytes()
-        for i in range( 2**2 ):
-            data += bytes(255) + bytes(0) + bytes(0)
-        print('data = ' + str(data))
-        im = Image.frombytes("RGB", (2,2), data)
-        im.save(filename, "PNG")
-
-    if (0):
-        print('write ' + str(width) + ' ' + str(height))
-        data = [(0, 0, 0) for x in range(width * height)]
-        for (x, y), v in iter(results.items()):
-            #print('x = ' + str(x))
-            #print(str(type(x)))
-            #print(str(type(y)))
-            #print(str(type(width)))
-            #z = x + y*width
-            #print(str(type(z)))
-            val = (
-                    int(255 * (1.0 - v)),
-                    int(255 * v),
-                    0
-                  )
-            data[x + y*width] = val
-            print(str(x + y*width) + ' = ' + str(val))
-        print('data = ' + str(data))
-        im = Image.new('L', (width, height))
-        im.putdata(data)
-        im.save(filename,"PNG")
-
-    if (0):
-        data = bytearray([0] * width * height)
-        for (x, y), v in iter(results.items()):
-            val = (
-                    int(255 * (1.0 - v)),
-                    int(255 * v),
-                    0
-                  )
-            data[x + y*width] = val
-        im = Image.frombytes('L', (width, height), str(data))
-        im.save(filename,"PNG")
 
 def transform(i, o):
     img = Image.open(i)
@@ -131,11 +87,64 @@ def transform(i, o):
             else:
                 red = intVal
                 green = 255
-            if (0 && (red, green, blue) != (r, g, b)):
+            if (0 and (red, green, blue) != (r, g, b)):
                 print('diff: ' + str((x, y, f)) + ' ' + str((red, green, blue)) + ' ' + str((r, g, b)))
             pix[x,y] = (red, green, blue)
 
     rgb_im.save(o, "PNG")
+
+def interpolate(r, t):
+    return r[0] * (1.0-t) + r[1] * t
+
+def interpolate_rgb(r, t):
+    return ( int(r[0][0] * (1.0-t) + r[1][0] * t + 0.5), int(r[0][1] * (1.0-t) + r[1][1] * t + 0.5), int(r[0][2] * (1.0-t) + r[1][2] * t + 0.5) )
+
+def my_hsv_to_rgb(h, s, v):
+    r, g, b = colorsys.hsv_to_rgb(h, s, v)
+    return (int(r * 255.0), int(g * 255.0), int(b * 255.0))
+
+def qtestImage(n, q):
+    img = Image.new('RGB', (q['numValues'], 1))
+    pix = img.load()
+    if (0):
+        ranges = []
+        pprev = None
+        for p in q['p']:
+            if (pprev):
+                ranges.append([pprev, p])
+            pprev = p
+        print('ranges = ' + str(ranges))
+
+    for i in range(1, 2 * q['numValues'], 2):  # sample midpoints of each quantized range
+        rangeLen = q['numValues'] / len(q['ranges'])
+        colorRange = int((i-1) // 2 / rangeLen)
+        #print('cr = ' + str(colorRange))
+        #colorRange = int(i * len(q['ranges']) / (2 * q['numValues']))
+        a = int(colorRange * rangeLen)
+        b = int((colorRange+1) * rangeLen)
+        t = ((i-1)//2 - a) / (b - a)
+        print('a,b,t = ' + str((a, b, t)))
+        if (q['mode'] == 'rgb'):
+            pix[(i-1) // 2, 0] = interpolate_rgb(q['ranges'][colorRange], t)
+            print('color at ' + str((i-1) // 2) + ' = ' + str(pix[(i-1) // 2, 0]))
+        elif (q['mode'] == 'hsv'):
+            if ('h' in q['blend']):
+                h = interpolate( (q['ranges'][colorRange][0][0], q['ranges'][colorRange][1][0]), t)
+            else:
+                h = q['ranges'][colorRange][0][0]
+            if ('s' in q['blend']):
+                s = interpolate( (q['ranges'][colorRange][0][1], q['ranges'][colorRange][1][1]), t)
+            else:
+                s = q['ranges'][colorRange][0][1]
+            if ('v' in q['blend']):
+                v = interpolate( (q['ranges'][colorRange][0][2], q['ranges'][colorRange][1][2]), t)
+            else:
+                v = q['ranges'][colorRange][0][2]
+            print('hsv = ' + str((h, s, v)))
+            pix[(i-1) // 2, 0] = my_hsv_to_rgb(h, s, v)
+        else:
+            raise Exception('unrecognized color mode')
+    img.save(n + '.png', 'PNG')
 
 if (__name__ == "__main__"):
     # main parser
@@ -151,11 +160,18 @@ if (__name__ == "__main__"):
     parser.add_argument('--help', action='store_true', help=argparse.SUPPRESS)
     parser.add_argument('-debug', action='store_true', help='print extra info')
     parser.add_argument('-input', type=str, help='input filename')
-    parser.add_argument('-out', type=str, help='optional output kmz name: <name>.kmz')
+    parser.add_argument('-out', type=str, help='optional output png name: <name>.png')
+    parser.add_argument('-q', type=str, help='choose the qunatized color scheme, default red_gree')
+    parser.add_argument('-qtest', action='store_true', help='output quantized color scheme gradient images')
     args = parser.parse_args()
 
     if (args.h or args.help):
         parser.print_help()
+        sys.exit(0)
+
+    if (args.qtest):
+        for n, q in iter(quant.items()):
+            qtestImage(n, q)
         sys.exit(0)
 
     if (0):
